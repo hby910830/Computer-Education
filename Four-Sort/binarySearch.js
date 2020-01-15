788f8c6624af2f3e1644ec8a2dcd873b5b963397
hanbaoyi <604594231@qq.com>
2020-01-15 20:45:33 +0800

    README.md 二分查找法-思路

--- Four-Sort/binarySearch.js
+++ Four-Sort/binarySearch.js
@@ -1,4 +1,10 @@
 //二分查找法
+/*思路：
+* 将 arr 中间的元素与 n 对比，如果相等就找到了
+* 如果比 n 大，就用 arr[0..mid]进行第一步
+* 如果比 n 小，就用 arr[mid..len]进行第一步
+* 直到数组长度缩为0，还找不到就是找不到了
+ */
 bSearch = (arr, n, start, end)=>{
 	if(end === start) return -1
 	let mid = parseInt((start + end) / 2) //某些语言会溢出
